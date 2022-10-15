4cebd905755a6c0d94b935587baad08e059909ab
PrachiBalpande <prajwalbalpande6@gmail.com>
2022-10-15 20:53:23 +0530

    hello

---
+++ problem.js
@@ -0,0 +1,12 @@
+let num=prompt("what is a official name of javascript ?")
+
+function problem1(){
+    let num=document.getElementById('ans').value;
+    if(num =="ECMAScript" || num=="A"){
+        document.write( "Right");
+    }
+    else{
+        document.write( "You don't Know answer");
+        
+    }
+}
